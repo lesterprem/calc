0b1db36a45f1cb0d7173d10c28893e11bfb29862
Author Name <your_public_email@example.com>
2024-01-06 23:08:10 +0100

    submitte dcalculator project v0

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-const buttonset=document.querySelector('buttoncontainer');
-for(let i=1;i<=9;i++){
+const buttonset=document.querySelector('.buttoncontainer');
+for(let i=15;i>=0;i--){
     btndiv=document.createElement('div');
     buttonset.appendChild(btndiv);
 }
+
+let result=0;
+let firstNum=[];
+let secondNum=[];
+let operator=""
+
+
+let buttonLabel=[1,2,3,'+',4,5,6,'-',7,8,9,'x',"0","C","=","/"];
+const buttons=Array.from(document.querySelectorAll('.buttoncontainer div'));
+
+buttons.forEach((item, index) => {
+    if(buttonLabel[index]){
+        item.textContent=buttonLabel[index];
+    }
+});
+
+buttons.forEach((item) =>{
+    currentNum=0;
+    item.addEventListener("click",(txt) =>{
+        if(Number.isInteger(txt.target.textContent)){
+            if(currentNum=0){
+                firstNum.push(txt.target.textContent);
+                console.log(firstNum);
+            } else if (currentNum=1){
+                secondNum.push(txt.target.textContent);
+                console.log(secondNum);
+            } 
+        }
+        else if (txt.target.textContent==="="){
+            evaluate();
+        } else{
+            if(currentNum=0){
+                operator=txt.target.textContent;
+            }
+            else{
+                disableOperators();
+            }
+        }
+    } );
+});
+
+function disableOperators(){
+    console.log("disableOperators");
+};
+function evaluate(){
+    console.log("evaluate");
+};
